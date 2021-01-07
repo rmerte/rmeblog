d778ece8caec9a09a7f809c5fbdccbccb9f3d224
rmerte <rmertemik@gmail.com>
2021-01-07 20:22:57 +0300

    bugfix

--- rmeblog/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-	session_start();
-	include 'login/ayar.php';
-	include 'login/ukas.php';
-
-	ukas_cikis("login/ayar.php", "index.php");
-	// Ayar dosyası, Çıkış yaptıktan sonra yönlendirilecek adres
-?>
